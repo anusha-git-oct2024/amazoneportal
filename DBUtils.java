56fbf3f5dad59b599864d71eb65b8a3fed711b87
devopstrainingblr <devopztrainingblr@gmail.com>
2024-10-10 19:29:19 +0000

    updated

--- DBUtils.java
+++ DBUtils.java
@@ -3,5 +3,5 @@ this files cantains db related code
 <<<<<<< HEAD
 hello added this line  ..
 hello .. added this line 
-=======
->>>>>>> parent of e9df01e (added deployment script)
+
+adding development branch
